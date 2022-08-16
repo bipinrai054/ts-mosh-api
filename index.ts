18dcd2ea238fa28c70ad8a0a6fa182d0e74978dc
bipnrai054 <bipinrai054@gmail.com>
2022-08-17 00:57:53 +0545

    minor routes done

--- index.ts
+++ index.ts
@@ -1,5 +1,14 @@
 import express from 'express'
 
+import remindersRouter from './routers/reminders'
+
 const app = express()
+
+
+app.use('/reminders', remindersRouter)
+
+app.get('/', (req, res) => {
+  res.send('Hello World')
+})
 
 app.listen(5000, () => console.log('Server Started!!'))
